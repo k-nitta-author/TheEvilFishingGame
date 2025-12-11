45d775e00bb2287cc82bdfb3c190915dab4695b7
Kennichi Nitta <k.nitta.author@gmail.com>
2025-12-11 13:43:55 +0800

    📨:services: added currency service

---
+++ services/CurrencyService.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+namespace Service
+{
+    // service for handling currency transacations in TEFG
+    public partial class CurrencyService: Resource
+    {
+        public int Balance {get; set;} // the player's current balance
+
+        // add to players currency balance
+        public void Add(int amount)
+        {
+            this.Balance += amount;
+        }
+    }   
+}
