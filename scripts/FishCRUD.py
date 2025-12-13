7c24a3afb9b87b567415fa0c72a911d9f0c80aeb
Kennichi Nitta <k.nitta.author@gmail.com>
2025-12-13 10:24:00 +0800

    📃:scripts: created scripts dir; FishCRUD.py - FishCRUD.py intended to manage JSON/ SQLite Data related to fish; also intended to export to CSV

---
+++ scripts/FishCRUD.py
@@ -0,0 +1,88 @@
+import csv
+import json
+import sqlite3
+import os.path
+import uuid
+
+DATA_DIRECTORY = ""
+dirname = os.path.abspath("../data")
+
+
+class Fish():
+
+    # TODO: Implement
+    def read_fish() -> str:
+        print("test")
+
+    # TODO: Implement
+    def create_fish() -> str:
+        
+        fish_data = {
+            "id": "", # USE UUID 
+            "name": input("Input Fish Name: "),
+            "strength": int(input("Input Fish Str: ")),
+            "speed": int(input("Input Fish Speed: ")),
+            "description": ""
+        }
+
+        return json.dumps(fish_data)
+
+    # TODO: Implement
+    def delete_fish() -> str:
+        pass
+
+    # TODO: Implement
+    def update_fish() -> str:
+        pass
+
+# TODO: Implement
+def initialize_fish_file():
+    
+    FILENAME = ""
+
+    full_path = os.path.join(DATA_DIRECTORY, FILENAME)
+
+    print(dirname)
+
+# TODO: Implement
+def parse_command(command: int):
+
+    print("What would you like to do?")
+
+    print('''
+1. Read Fish Data
+2. Create New Fish Data
+3. Update Fish Data
+4. Delete Fish Data
+5. Initialize Fish File
+6. Exit
+          ''')
+
+    command_dict = {
+        1: Fish.read_fish,
+        2: Fish.create_fish,
+        3: Fish.update_fish,
+        4: Fish.delete_fish,
+        5: initialize_fish_file,
+        6: exit
+    }
+
+    return command_dict[command]
+
+if __name__ == "__main__":
+
+    print("Welcome to Fish Editor")
+
+    print('''
+1. Read Fish Data
+2. Create New Fish Data
+3. Update Fish Data
+4. Delete Fish Data
+5. Initialize Fish File
+6. Exit
+          ''')
+
+    while True:
+        c = parse_command(int(input()))
+
+        c()
